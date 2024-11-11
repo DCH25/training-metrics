b5bc7137714ce08c444aa1b6b05ed048a123b5c8
Daniel Chua <53444908+DCH25@users.noreply.github.com>
2024-11-10 19:07:34 -0500

    feat: set up page routes

--- training-metrics/src/app/app.routes.ts
+++ training-metrics/src/app/app.routes.ts
@@ -1,9 +1,24 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { MetricsComponent } from './metrics/metrics.component';
+import { ProgramsComponent } from './programs/programs.component';
+import { SettingsComponent } from './settings/settings.component';
 
 export const routes: Routes = [
-    {
-        path: 'dashboard',
-        component: DashboardComponent,
-    }
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+  },
+  {
+    path: 'metrics',
+    component: MetricsComponent,
+  },
+  {
+    path: 'programs',
+    component: ProgramsComponent,
+  },
+  { 
+    path: 'settings',
+    component: SettingsComponent,
+  },
 ];
